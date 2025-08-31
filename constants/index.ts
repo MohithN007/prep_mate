9a0c66d99776970ab67db28d77f96ddfb8221168
Your Name MohithN007 <mohithn077@gmail.com>
2025-09-01 01:38:27 +0530

    implemented feedback and waiting for future updates

--- constants/index.ts
+++ constants/index.ts
@@ -157,33 +157,33 @@ End the conversation on a polite and positive note.
 
 export const feedbackSchema = z.object({
   totalScore: z.number(),
-  categoryScores: z.tuple([
-    z.object({
+  categoryScores: z.object({
+    communicationSkills: z.object({
       name: z.literal("Communication Skills"),
       score: z.number(),
       comment: z.string(),
     }),
-    z.object({
+    technicalKnowledge: z.object({
       name: z.literal("Technical Knowledge"),
       score: z.number(),
       comment: z.string(),
     }),
-    z.object({
+    problemSolving: z.object({
       name: z.literal("Problem Solving"),
       score: z.number(),
       comment: z.string(),
     }),
-    z.object({
+    culturalFit: z.object({
       name: z.literal("Cultural Fit"),
       score: z.number(),
       comment: z.string(),
     }),
-    z.object({
+    confidenceAndClarity: z.object({
       name: z.literal("Confidence and Clarity"),
       score: z.number(),
       comment: z.string(),
     }),
-  ]),
+  }),
   strengths: z.array(z.string()),
   areasForImprovement: z.array(z.string()),
   finalAssessment: z.string(),
